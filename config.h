2542ceb7b406ee0e8dab96f95f41434ff83192fd
Thang Nguyen <tnguye20@uvm.edu>
2020-10-08 09:17:17 -0400

    Merged with Luke's changes

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Medium:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Iosevka Medium:pixelsize=17:antialias=true:autohint=true";
 /* static char *font2[] = { "Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true" }; */
-static char *font2[] = { "Source Code Pro:pixelsize=18:antialias=true:autohint=true" };
+static char *font2[] = { "Source Code Pro:pixelsize=17:antialias=true:autohint=true" };
 static int borderpx = 3;
 
 /*
@@ -109,7 +109,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
