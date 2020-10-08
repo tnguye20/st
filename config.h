cc97b25596cfb783c50336293fb1c7396033e203
Thang Nguyen <tnguye20@uvm.edu>
2020-10-08 08:54:12 -0400

    Alter font size

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Medium:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Iosevka Medium:pixelsize=18:antialias=true:autohint=true";
 /* static char *font2[] = { "Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true" }; */
-static char *font2[] = { "Source Code Pro:pixelsize=16:antialias=true:autohint=true" };
+static char *font2[] = { "Source Code Pro:pixelsize=18:antialias=true:autohint=true" };
 static int borderpx = 3;
 
 /*
