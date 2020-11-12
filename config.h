7f20983f1202c1e3883b678aaf42c4c6c63b465d
Thang Nguyen <tnguye20@uvm.edu>
2020-11-12 09:11:01 -0500

    Update font and size

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Medium:pixelsize=17:antialias=true:autohint=true";
+static char *font = "Iosevka Medium:pixelsize=20:antialias=true:autohint=true";
 /* static char *font2[] = { "Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true" }; */
-static char *font2[] = { "Source Code Pro:pixelsize=17:antialias=true:autohint=true" };
+static char *font2[] = { "Iosevka Medium:pixelsize=20:antialias=true:autohint=true" };
 static int borderpx = 3;
 
 /*
